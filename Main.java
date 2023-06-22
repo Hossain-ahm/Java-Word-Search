45796ba9ca98a786ca3f69caa9778e08062b5284
Hossain Ahmed <hossainahmed2002@gmail.com>
2023-06-22 18:08:03 +0100

    Finsh the search class for finding words

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
+
 public class Main {
   public static void main(String[] args) {
+    Search search = new Search();
     System.out.println("Hello World");
+    ArrayList<String> list = new ArrayList<String>();
+    list = search.pool("hbslrmodiact");
+    list = search.combine(list, search.prefix("c"));
+    search.printArrayList(list);
+
   }
+
+
 }
--- Search.java
+++ Search.java
@@ -1,17 +1,16 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class Search {
-    private ArrayList<String> words;
+    private ArrayList<String> words;//An arraylist that will hold all words
 
     public Search() {
         words = new ArrayList<String>();
         try {
+            //Read a text file that contains all the words and add them to the arraylist
             File wordsAplha = new File("words_alpha.txt");
             Scanner reader = new Scanner(wordsAplha);
             while (reader.hasNextLine()) {
@@ -19,12 +18,14 @@ public class Search {
                 words.add(word);
             }
             reader.close();
+
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
     }
 
+    //this function will return all words that begin with a given string
     public ArrayList<String> prefix (String prefix) {
         ArrayList<String> newWords = new ArrayList<String>();
 
@@ -37,6 +38,7 @@ public class Search {
 
     }
 
+    //this function will return all words that ends with a given string
     public ArrayList<String> suffix (String suffix) {
         ArrayList<String> newWords = new ArrayList<String>();
 
@@ -48,6 +50,8 @@ public class Search {
         return newWords;
 
     }
+
+    //this function will return all words that contains a given string
     public ArrayList<String> containsInOrder (String content) {
         ArrayList<String> newWords = new ArrayList<String>();
         for (String word : this.words) {
@@ -59,6 +63,7 @@ public class Search {
 
     }
 
+    // a function that will return a hash map that counts the number of occrence of the characters in
     public HashMap<Character,Integer> charCount (String content) {
         HashMap<Character,Integer> map = new HashMap<Character,Integer>();
 
@@ -73,49 +78,107 @@ public class Search {
         return map;
     }
 
-    public ArrayList<String> contains (String content) {
+    //a function that will returns word that can be made from a pool of letters
+    public ArrayList<String> pool (String content) {
         ArrayList<String> newWords = new ArrayList<String>();
-        ArrayList<String> newWords2 = new ArrayList<String>();
-        newWords2.addAll(words);
         HashMap<Character,Integer> map = charCount(content);
 
         for (String word :this.words){
             HashMap<Character,Integer> wordMap = charCount(word);
-            for (Character c :map.keySet()){
-                if (!(map.get(c).equals(wordMap.get(c)))){
-                    newWords.add(word);
+
+            boolean canMakeCurrentWord = true;
+            for (Character c : wordMap.keySet()){
+                int currentWordCharCount = wordMap.get(c);
+                int letterCharCount = 0;
+                if (map.containsKey(c)){
+                    letterCharCount = map.get(c);
+                }
+                if (currentWordCharCount > letterCharCount){
+                    canMakeCurrentWord = false;
                     break;
                 }
             }
+            if (canMakeCurrentWord){
+                newWords.add(word);
+            }
         }
 
-        for (String word : newWords){
-            newWords2.remove(word);
-        }
- 
-
-        return newWords2;
-
+        return newWords;
     }
 
+    //a function that will returns word that must contain the given letter
+    public ArrayList<String> contains (String content) {
+        ArrayList<String> newWords = new ArrayList<String>();
+        HashMap<Character,Integer> map = charCount(content);
 
+        for (String word :this.words){
+            HashMap<Character,Integer> wordMap = charCount(word);
+
+            boolean canMakeCurrentWord = true;
+            for (Character c : map.keySet()){
+                int currentWordCharCount = 0;
+                int letterCharCount = map.get(c);
+                if (wordMap.containsKey(c)){
+                    currentWordCharCount = wordMap.get(c);
+                }
+                if (currentWordCharCount < letterCharCount){
+                    canMakeCurrentWord = false;
+                    break;
+                }
+            }
+            if (canMakeCurrentWord){
+                newWords.add(word);
+            }
+        }
+
+        return newWords;
+    }
+
+    public ArrayList<String> exactLength(int len) {
+        ArrayList<String> newWords = new ArrayList<String>();
+        for (String word : this.words) {
+            if (word.length() == len){
+                newWords.add(word);
+            }
+
+        }
+
+        return newWords;
+    }
+    public ArrayList<String> lessLength(int len) {
+        ArrayList<String> newWords = new ArrayList<String>();
+        for (String word : this.words) {
+            if (word.length() <= len){
+                newWords.add(word);
+            }
+
+        }
+
+        return newWords;
+    }
+    public ArrayList<String> greaterLength(int len) {
+        ArrayList<String> newWords = new ArrayList<String>();
+        for (String word : this.words) {
+            if (word.length() >= len){
+                newWords.add(word);
+            }
+
+        }
+
+        return newWords;
+    }
+
+    //this combine two list of words returning the same words they both contain
     public ArrayList<String> combine (ArrayList<String> array1, ArrayList<String> array2) {
         array1.retainAll(array2);
         return array1;
 
     }
 
-    public static void main(String[] args) {
-
-
-        Search search = new Search();
-        ArrayList<String> words = search.contains("aaelio");
- 
-
-        for (String word : words) {
+    public void printArrayList(ArrayList<String> arrayList) {
+        for (String word : arrayList) {
             System.out.println(word);
         }
     }
-
 
 }
